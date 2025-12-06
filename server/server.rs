6c94fa46261bdaa208985cb4e117268d2e971044
kai <chenkenny414@gmail.com>
2025-12-06 18:07:17 -0500

    basic GET and POST for tcp stream

---
+++ server/server.rs
@@ -0,0 +1,31 @@
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines() //returns Result<String, std::io::Error> by splitting on \n
+        .map(|result| result.unwrap()) //extract the string, if runs into error, unwrap
+        .take_while(|line| !line.is_empty()) //stop at the first empty line
+        .collect(); //collect into a vector
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("../static/hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}

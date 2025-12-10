b5f038f258b1ae7b634b412f7575f45dfc1ceea0
kai <chenkenny414@gmail.com>
2025-12-09 20:32:37 -0500

    simplified if else statement

--- server/server.rs
+++ server/server.rs
@@ -2,26 +2,34 @@ use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use arcsin::server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let _http_request = buf_reader.lines().next().unwrap().unwrap();
 
-    let(status_line, file_name) = if _http_request == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "../static/hello.html")
-    }
-    else{
-        ("HTTP/1.1 404 NOT FOUND", "../static/404.htmk")
+    let(status_line, file_name) = match &_http_request[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "../static/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "../static/hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "../static/404.html"),
     };
 
     let contents = fs::read_to_string(file_name).unwrap();

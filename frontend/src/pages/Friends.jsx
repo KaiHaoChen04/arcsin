efd2afa56eb72bb21d8b724bc07dc1cdc5213f87
kai <chenkenny414@gmail.com>
2026-01-28 21:39:13 -0500

    fetch friends useEffect

--- frontend/src/pages/Friends.jsx
+++ frontend/src/pages/Friends.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState} from 'react';
+import axios from 'axios';
+
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        fetchFriends();
+    }, []);
+
+    const fetchFriends = async () => {
+        try {
+            const res = await axios.get("/api/friends");
+            setFriends(res.data);
+        } catch (error) {
+            console.log("Error fetching friends: ", error);
+        }
+    }
     return (
+
         <div>
             <h1>Friends</h1>
         </div>
